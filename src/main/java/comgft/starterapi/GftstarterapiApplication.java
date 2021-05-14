45d7ee3af8ca2ab66005d61f4ed9b5394bae7723
dodn <diego.lourenco@gft.com>
2021-05-14 18:08:34 -0300

    Initial commit

---
+++ src/main/java/comgft/starterapi/GftstarterapiApplication.java
@@ -0,0 +1,13 @@
+package comgft.starterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GftstarterapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GftstarterapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comgft/starterapi/GftstarterapiApplicationTests.java
@@ -0,0 +1,13 @@
+package comgft.starterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GftstarterapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
